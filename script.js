32fd948239db3c1bb20b43eaf9d8cca4ac0c64a9
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-06 11:44:06 -0400

    Made variables for the elements that will be used alse added the function to switch the calculator between light and dark modes

---
+++ script.js
@@ -0,0 +1,48 @@
+// selects the elements on the page that will be used
+const calculator = document.querySelector(".calculator-body");
+const screen = document.querySelector(".calculator-screen");
+
+const numbers = document.querySelectorAll(".number");
+numbers.forEach(number => {
+    number.addEventListener("click", clickedNumber);
+});
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(operator => {
+    operator.addEventListener("click", clikcedOperator);
+});
+
+const lightDarkToggle = document.querySelector(".toggle");
+lightDarkToggle.addEventListener("click", toggleLightDarkMode);
+
+// toggles between dark and light mode when the toggle switch is pressed
+function toggleLightDarkMode() {
+    if (calculator.classList.contains("light-calculator")) {
+        calculator.classList.remove("light-calculator");
+        numbers.forEach(number => {
+            number.classList.remove("light-number");
+        });
+        operators.forEach(operator => {
+            operator.classList.remove("light-operator");
+        });
+    }
+    else {
+        calculator.classList.add("light-calculator");
+        numbers.forEach(number => {
+            number.classList.add("light-number");
+        });
+        operators.forEach(operator => {
+            operator.classList.add("light-operator");
+        });
+    }
+}
+
+// placeholder function
+function clickedNumber() {
+    console.log(this.id);
+}
+
+// placeholder function
+function clikcedOperator() {
+    console.log(this.id);
+}
